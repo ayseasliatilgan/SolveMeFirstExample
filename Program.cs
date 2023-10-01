de4a8fa0b74102ece9f91f444b3c008086ff762a
aasli <aasli@ASLINB>
2023-10-01 23:19:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int solveMeFirst(int a, int b)
+        {
+            // Hint: Type return a+b; below 
+            int sum = a + b;
+            if (a >= 1 && a <= 1000 && b >= 1 && b <= 1000)
+                
+            {
+                sum = a + b;
+            }
+
+            return sum;
+
+        }
+
+        int a = 2;
+        int b = 3;
+
+        int result = a + b;
+        Console.WriteLine(result);
+
+
+
+    }
+}
